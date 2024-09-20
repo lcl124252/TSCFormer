d4a52a7c89eb6963abdb1232efa4b5e363d76d63
li changliang <1242520599@qq.com>
2024-09-20 13:00:30 +0800

    TSCFormer

--- packages/DFA3D/build/lib.linux-x86_64-3.8/dfa3D/ops/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .multi_scale_3D_deform_attn import MultiScaleDepthScoreSampleFunction, WeightedMultiScaleDeformableAttnFunction
-
-__all__ = [
-    'MultiScaleDepthScoreSampleFunction', 'WeightedMultiScaleDeformableAttnFunction'
-]
